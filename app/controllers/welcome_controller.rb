0593b97ea5176058857ca1da3480a497359ef763
Sam Ruby <ruby@intertwingly.net>
2015-04-19 13:44:21 +0530

    new forms

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+def vali
+end
+
+  def create
+  end
+
+  def update
+  end
+
+  def view
+  end
+end
---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+  test "should get view" do
+    get :view
+    assert_response :success
+  end
+
+end
